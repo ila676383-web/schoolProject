ec505647a55aab5fd4f9a2cc4359fafeeb83480f
ila676383-web <ila676383@gmail.com>
2025-12-29 00:53:16 +0500

    Fix admin access SSR

--- src/modules/AdminControl.tsx
+++ src/modules/AdminControl.tsx
@@ -1,17 +1,23 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import ControlPanel from "./ControlPanel";
 
 const AdminControl = () => {
-  const isOk = localStorage.getItem("password");
-  return (
-    <>
-      {isOk ? (
-        <ControlPanel />
-      ) : (
-        <h1 className="m-auto mt-48 text-4xl font-extrabold">Нет доступа!</h1>
-      )}
-    </>
+  const [isOk, setIsOk] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsOk(!!token);
+  }, []);
+
+  if (isOk === null) return null;
+
+  return isOk ? (
+    <ControlPanel />
+  ) : (
+    <h1 className="m-auto mt-48 text-4xl font-extrabold">Нет доступа!</h1>
   );
 };
+
 export default AdminControl;
